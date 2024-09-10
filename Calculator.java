a900d755f15fa3a379dc747ace9c591a37f069fe
ErcelJohn <erceljohntolentino@gmail.com>
2024-09-10 22:09:28 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,9 +4,13 @@ public class calculator {
     int b = 4;
     int sum = a + b;
     int sub = a - b;
+    int mul = a * b;
+    int quo = a / b;
     
     System.out.println("Addition: " + sum);
     System.out.println("subtraction: " + sub);
+    System.out.println("multiplication: " + mul);
+    System.out.println("division: " + quo);
 
   }
 }
