0357d9c88a21d4aec17f06bf4aaecb0803bc5b9e
ErcelJohn <erceljohntolentino@gmail.com>
2024-09-10 22:01:30 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,10 @@ public class calculator {
     int a = 2;
     int b = 4;
     int sum = a + b;
-
+    int sum = a - b;
+    
     System.out.println("Addition: " + sum);
+    System.out.println("subtraction: " + sub;
 
   }
 }
